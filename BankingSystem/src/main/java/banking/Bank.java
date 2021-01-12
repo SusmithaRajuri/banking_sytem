9ce5c1a6db1dedea992318352746bfe4878711a0
Susmitha_Rajuri <susmitharajuri97@gmail.com>
2021-01-12 23:05:02 +0530

    Update Bank.java

--- BankingSystem/src/main/java/banking/Bank.java
+++ BankingSystem/src/main/java/banking/Bank.java
@@ -10,40 +10,52 @@ public class Bank implements BankInterface {
 	private LinkedHashMap<Long, Account> accounts;
 
 	public Bank() {
-		// complete the function
 	}
 
 	private Account getAccount(Long accountNumber) {
-		// complete the function
-        return null;
+        return accounts.get(accountNumber);
 	}
 
 	public Long openCommercialAccount(Company company, int pin, double startingDeposit) {
-		// complete the function
-        return -1L;
+		Long accountNumber = Long.valueOf(accounts.size() + 1);
+		CommercialAccount commercialAccount = new CommercialAccount(company, accountNumber, pin, startingDeposit);
+		accounts.put(accountNumber, commercialAccount);
+		return accountNumber;
 	}
 
 	public Long openConsumerAccount(Person person, int pin, double startingDeposit) {
-		// complete the function
-        return -1L;
+		Long accountNumber = Long.valueOf(accounts.size() + 1);
+		ConsumerAccount consumerAccount = new ConsumerAccount(person, accountNumber, pin, startingDeposit);
+		accounts.put(accountNumber, consumerAccount);
+		return accountNumber;
 	}
 
 	public boolean authenticateUser(Long accountNumber, int pin) {
-		// complete the function
-        return true;
+		Account account = accounts.get(accountNumber);
+		if (null == account) {
+			return false;
+		}
+        return account.validatePin(pin);
 	}
 
 	public double getBalance(Long accountNumber) {
-		// complete the function
-        return -1;
+		Account account = accounts.get(accountNumber);
+        return null == account ? -1 : account.getBalance();
 	}
 
 	public void credit(Long accountNumber, double amount) {
-		// complete the function
+		Account account = accounts.get(accountNumber);
+		if (null == account) {
+			return;
+		}
+		account.creditAccount(amount);
 	}
 
 	public boolean debit(Long accountNumber, double amount) {
-		// complete the function
-        return true;
+		Account account = accounts.get(accountNumber);
+		if (null == account) {
+			return  false;
+		}
+		return amount <= account.getBalance();
 	}
 }
